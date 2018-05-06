5791589ff4da2811f4ed0cd5fd5440666af4d8e3
Matthew Collins <mcollins@acis.ufl.edu>
2018-05-06 13:31:55 -0400

    Added computation test of Spark, not just context instantiation

--- env_test.py
+++ env_test.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-activate_this_file = "/home/mcollins/spark_keras2/bin/activate_this.py"
-with open(activate_this_file) as f:
-    exec(f.read(), {'__file__': activate_this_file})
+#activate_this_file = "/home/mcollins/spark_keras2/bin/activate_this.py"
+#with open(activate_this_file) as f:
+#    exec(f.read(), {'__file__': activate_this_file})
 
 
 #os.environ["PYSPARK_PYTHON"] = "spark_keras2/bin/python"
@@ -22,3 +22,22 @@ print(sqlContext)
 #sys.path.insert(0, "deps.zip")
 
 import h5py
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+model = Sequential()
+
+
+# Spark work test
+
+import random
+NUM_SAMPLES = 1000
+def inside(p):
+    x, y = random.random(), random.random()
+    return x*x + y*y < 1
+
+count = sc.parallelize(range(0, NUM_SAMPLES)) \
+             .filter(inside).count()
+
+print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))

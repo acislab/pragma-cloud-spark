33c0e4f825a8936e5531a6f223824fb285c0002e
Matthew Collins <mcollins@acis.ufl.edu>
2018-05-06 11:27:57 -0700

    test of elephas, nearly working, training at the last phases has misaligned ml.vector and mllib.vector error

--- elephas_test.py
+++ elephas_test.py
@@ -1,5 +1,12 @@
+#https://github.com/maxpumperla/elephas/blob/master/examples/Spark_ML_Pipeline.ipynb
+
 import os
 import sys
+
+activate_this_file = "/home/mcollins/spark_keras2/bin/activate_this.py"
+with open(activate_this_file) as f:
+    exec(f.read(), {'__file__': activate_this_file})
+
 from pyspark.context import SparkContext
 from pyspark.sql import SQLContext
 sc = SparkContext()
@@ -7,7 +14,7 @@ sqlContext = SQLContext(sc)
 print(sqlContext)
 
 
-from pyspark.mllib.linalg import Vectors
+from pyspark.ml.linalg import Vectors
 import numpy as np
 import random
 
@@ -15,11 +22,6 @@ sql_context = SQLContext(sc)
 
 data_path = "data"
 
-df = sc.textFile(os.path.join(data_path, "test.csv"))
-
-print(df.count())
-
-sys.exit(0)
 
 # This happens locally
 def shuffle_csv(csv_file):
@@ -50,3 +52,93 @@ train_df.show(10)
 
 print("Test data frame (note the dummy category):")
 test_df.show(10)
+
+
+from pyspark.ml.feature import StringIndexer
+
+string_indexer = StringIndexer(inputCol="category", outputCol="index_category")
+fitted_indexer = string_indexer.fit(train_df)
+indexed_df = fitted_indexer.transform(train_df)
+
+from pyspark.ml.feature import StandardScaler
+
+scaler = StandardScaler(inputCol="features", outputCol="scaled_features", withStd=True, withMean=True)
+fitted_scaler = scaler.fit(indexed_df)
+scaled_df = fitted_scaler.transform(indexed_df)
+
+print("The result of indexing and scaling. Each transformation adds new columns to the data frame:")
+scaled_df.show(10)
+
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils, generic_utils
+
+nb_classes = train_df.select("category").distinct().count()
+input_dim = len(train_df.select("features").first()[0])
+
+model = Sequential()
+model.add(Dense(512, input_shape=(input_dim,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+
+
+
+from elephas.ml_model import ElephasEstimator
+from elephas import optimizers as elephas_optimizers
+
+# Define elephas optimizer (which tells the model how to aggregate updates on the Spark master)
+adadelta = elephas_optimizers.Adadelta()
+
+# Initialize SparkML Estimator and set all relevant properties
+estimator = ElephasEstimator()
+estimator.setFeaturesCol("scaled_features")             # These two come directly from pyspark,
+estimator.setLabelCol("index_category")                 # hence the camel case. Sorry :)
+estimator.set_keras_model_config(model.to_yaml())       # Provide serialized Keras model
+estimator.set_optimizer_config(adadelta.get_config())   # Provide serialized Elephas optimizer
+estimator.set_categorical_labels(True)
+estimator.set_nb_classes(nb_classes)
+estimator.set_num_workers(1)  # We just use one worker here. Feel free to adapt it.
+estimator.set_nb_epoch(20) 
+estimator.set_batch_size(128)
+estimator.set_verbosity(1)
+estimator.set_validation_split(0.15)
+
+
+
+
+from pyspark.ml import Pipeline
+
+pipeline = Pipeline(stages=[string_indexer, scaler, estimator])
+
+
+fitted_pipeline = pipeline.fit(train_df) # Fit model to data
+
+prediction = fitted_pipeline.transform(train_df) # Evaluate on train data.
+# prediction = fitted_pipeline.transform(test_df) # <-- The same code evaluates test data.
+pnl = prediction.select("index_category", "prediction")
+pnl.show(100)
+
+prediction_and_label = pnl.map(lambda row: (row.index_category, row.prediction))
+
+prediction_and_label.head(10)
+
+
+
+
+
+#from pyspark.mllib.evaluation import MulticlassMetrics
+#metrics = MulticlassMetrics(prediction_and_label)
+#print(metrics.precision())
+
